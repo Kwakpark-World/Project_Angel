520874f8b6b4dd8f52820aeab7c344d63f70e2e9
김준호 <kjhlove0119@naver.com>
2024-01-26 16:31:13 +0900

    FEAT Player

---
+++ Assets/01.Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerStateEnum
+{
+    Idle,
+    Move,
+    Fall,
+    Dash,
+    MeleeAttack,
+}
+
+public class PlayerStateMachine
+{
+    public PlayerState CurrentState { get; private set; }
+    public Dictionary<PlayerStateEnum, PlayerState> StateDictionary = new Dictionary<PlayerStateEnum, PlayerState>();
+
+    private Player _player;
+
+    public void Initialize(PlayerStateEnum startState, Player player)
+    {
+        _player = player;
+        CurrentState = StateDictionary[startState];
+        CurrentState.Enter();
+    }
+
+    public void AddState(PlayerStateEnum state, PlayerState playerState)
+    {
+        StateDictionary.Add(state, playerState);
+    }
+
+    public void ChangeState(PlayerStateEnum state)
+    {
+        // 플레이어가  맞고 있거나 뭔가 일이 있어서 상태 전환하지 못하는 경우
+
+        CurrentState.Exit();
+        CurrentState = StateDictionary[state];
+        CurrentState.Enter();
+    }
+}
