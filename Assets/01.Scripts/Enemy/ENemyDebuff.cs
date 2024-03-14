e0755b9688761e44db3369749ce89028ce3c0f1c
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-03-14 10:51:53 +0900

    FIX _EnemyAi

--- Assets/01.Scripts/Enemy/ENemyDebuff.cs
+++ Assets/01.Scripts/Enemy/ENemyDebuff.cs
@@ -8,6 +8,8 @@ public class ENemyDebuff : PoolableMono
 
     private float poisonDamage = 2f;
 
+    private float speed = 3f;
+    private Rigidbody rb;
     public DebuffType _debuffType;
 
     public enum DebuffType
@@ -17,6 +19,32 @@ public class ENemyDebuff : PoolableMono
         push
     }
 
+    void Update()
+    {
+        // 플레이어를 향해 회전
+        Vector3 direction = (GameManager.Instance.player.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 7); // 회전 속도를 고정값으로 설정
+
+        // 일직선 운동
+        Vector3 initialVelocity = CalculateInitialVelocity(GameManager.Instance.player.transform.position, transform.position, speed);
+        rb.velocity = initialVelocity;
+    }
+
+    // 일직선 운동을 위한 초기 속도 계산
+    Vector3 CalculateInitialVelocity(Vector3 targetPosition, Vector3 currentPosition, float speed)
+    {
+        // 수평 거리 계산
+        Vector3 displacementXZ = new Vector3(targetPosition.x - currentPosition.x, 0, targetPosition.z - currentPosition.z);
+
+        // 수평 속도 계산
+        Vector3 velocityXZ = displacementXZ.normalized * speed;
+
+        // 수직 속도는 고려하지 않음 (일직선 운동)
+        Vector3 initialVelocity = velocityXZ;
+
+        return initialVelocity;
+    }
 
     public void PoisonPortion()
     {   
