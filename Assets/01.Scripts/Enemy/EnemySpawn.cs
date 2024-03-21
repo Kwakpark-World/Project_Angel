c1fc66a93281eebe4ad513efcd1745f15d268794
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-03-21 09:32:05 +0900

    FIX _EnemySpawn

--- Assets/01.Scripts/Enemy/EnemySpawn.cs
+++ Assets/01.Scripts/Enemy/EnemySpawn.cs
@@ -17,21 +17,46 @@ public class EnemySpawn : MonoBehaviour
         knightEnemySpawn();
     }
 
+    public void Start()
+    {
+        
+    }
+
     public void knightEnemySpawn()
     {
-        if (EnemySpawnCount < 15) 
+        if (EnemySpawnCount < 7)
         {
-            // 무작위 소환 위치 계산
             Vector3 spawnPosition = new Vector3(
                 Random.Range(MinSpawnValue.x, MaxSpawnValue.x),
                 Random.Range(MinSpawnValue.y, MaxSpawnValue.y),
                 Random.Range(MinSpawnValue.z, MaxSpawnValue.z)
             );
 
-            EnemyBrain enemy = PoolManager.Instance.Pop(PoolingType.KnightEnemy) as EnemyBrain; 
+            // 랜덤으로 적의 종류 선택
+            float randomValue = Random.value;  // 0: 기사, 1: 궁수, 2: 마법사
 
-            enemy.transform.position = spawnPosition;
-            EnemySpawnCount++;
+            EnemyBrain enemy = null;
+
+
+            if (randomValue < 0.3f) // 30% 확률로 기사
+            {
+                enemy = PoolManager.Instance.Pop(PoolingType.KnightEnemy) as EnemyBrain;
+            }
+            else if (randomValue < 0.6f) // 30% 확률로 궁수
+            {
+                enemy = PoolManager.Instance.Pop(PoolingType.ArcherEnemy) as EnemyBrain;
+            }
+            else // 나머지 확률로 마법사
+            {
+                enemy = PoolManager.Instance.Pop(PoolingType.WitchEnemy) as EnemyBrain;
+            }
+
+
+            if (enemy != null)
+            {
+                enemy.transform.position = spawnPosition;
+                EnemySpawnCount++;
+            }
         }
 
     }
