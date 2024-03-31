961f70c317d6d7f423ed89b2007aac9124c69de2
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-04-01 00:52:26 +0900

    FEAT AwakenBar

--- Assets/01.Scripts/UI/PlayerHpUI.cs
+++ Assets/01.Scripts/UI/PlayerHpUI.cs
@@ -7,12 +7,31 @@ using UnityEngine.UI;
 public class PlayerHpUI : MonoBehaviour
 {
     public Slider _maxHpBar;
+
+    public Slider _maxidentityBar;
     public Slider _identityBar;
 
     public PlayerController _playerController;
+    public Player _player;
     
     private void Update()
     {
+        PlayerHP();
+        PlayerAwaken();
+    }
+
+    public void PlayerHP()
+    {
         _maxHpBar.value = _playerController.CurrentHealth;
+    }
+
+    public void PlayerAwaken()
+    {
+        _identityBar.value = _player.awakenCurrentGage;
+
+        if(_maxidentityBar.value == 100)
+        {
+            _identityBar.value -= 1;
+        }
     }
 }
