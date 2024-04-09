81286610e8d72f7760dcf8bf609e7b3350fc6c64
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-04-09 12:12:12 +0900

    FIX SoundManager

--- Assets/01.Scripts/Core/SoundManager.cs
+++ Assets/01.Scripts/Core/SoundManager.cs
@@ -1,49 +1,84 @@
-using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
-using UnityEngine.Events;
-using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
-public class SoundManager : MonoBehaviour
+public enum SoundMode
 {
-    private static SoundManager instance;
+    None,
+    Intro,
+    NonCombat,
+    Combat
+}
 
-    public AudioSource bgmSound;
-    public AudioClip[] bgmList;
+public enum SoundEffectType
+{
+    Button,
+    Scroll,
+    Buy,
+}
+
+[Serializable]
+public struct SoundModeSource
+{
+    public SoundMode mode;
+    public AudioSource source;
+}
+
+[Serializable]
+public struct SoundEffectClip
+{
+    public SoundEffectType mode;
+    public AudioClip clip;
+}
+
+public class SoundManager : MonoSingleton<SoundManager>
+{
+    public SoundModeSource[] bgmList;
+    public SoundEffectClip[] sfxList;
+    public AudioMixerGroup sfxGroup;
+    private Dictionary<SoundMode, AudioSource> _bgmDictionary = new Dictionary<SoundMode, AudioSource>();
+    private Dictionary<SoundEffectType, AudioClip> _sfxDicionary = new Dictionary<SoundEffectType, AudioClip>();
+    private SoundMode _soundMode = SoundMode.None;
 
     private void Awake()
     {
-        if(instance == null)
+        foreach (SoundModeSource bgm in bgmList)
         {
-            instance = this;
-            SceneManager.sceneLoaded += OnSceneload;
+            _bgmDictionary.Add(bgm.mode, bgm.source);
         }
 
-        else if(instance != null)
+        foreach (SoundEffectClip bgm in sfxList)
         {
-            Destroy(gameObject);
+            _sfxDicionary.Add(bgm.mode, bgm.clip);
         }
 
         DontDestroyOnLoad(gameObject);
     }
 
-    private void OnSceneload(Scene arg0, LoadSceneMode arg1)
+    public void ChangeBGMMode(SoundMode mode)
     {
-        for(int i =0; i < bgmList.Length; i++)
+        if (_soundMode != SoundMode.None)
         {
-            if(arg0.name == bgmList[i].name)
-            {
-                BGMPlay(bgmList[i]);
-            }
+            _bgmDictionary[_soundMode].Stop();
         }
+
+        _soundMode = mode;
+
+        _bgmDictionary[_soundMode].Play();
     }
 
-    private void BGMPlay(AudioClip audioClip)
+    public void PlaySFX(SoundEffectType mode)
     {
-        bgmSound.clip = audioClip;
-        bgmSound.loop = true;
-        bgmSound.volume = 1.0f;
-        bgmSound.Play();
+        GameObject gameObject = new GameObject("One shot audio");
+        gameObject.transform.position = transform.position;
+        AudioSource audioSource = (AudioSource)gameObject.AddComponent(typeof(AudioSource));
+        audioSource.clip = _sfxDicionary[mode];
+        audioSource.outputAudioMixerGroup = sfxGroup;
+        audioSource.Play();
+        Destroy(gameObject, _sfxDicionary[mode].length * ((Time.timeScale < 0.01f) ? 0.01f : Time.timeScale));
     }
 }
