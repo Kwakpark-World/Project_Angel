872e85a590a28d1df08a2b04e6d78076b97cd07a
김준호 <kjhlove0119@naver.com>
2024-05-02 09:57:17 +0900

    FIX Player Prefab

--- Assets/01.Scripts/Player/State/Attack/PlayerNormalChargeAttackState.cs
+++ Assets/01.Scripts/Player/State/Attack/PlayerNormalChargeAttackState.cs
@@ -1,22 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerNormalChargeAttackState : PlayerChargeState
 {
-    private float _normalDist = 6;
-    private float _awakenDist = 10;
-
     private float _normalWidth = 11f;
     private float _awakenWidth = 11f;
 
     private float _normalHeight = 3f;
     private float _awakenHeight = 3f;
 
-    private Vector3 _normalAttackOffset = new Vector3(0f, 0f, 4.25f);
+    private float _normalDist = 6;
+    private float _awakenDist = 10;
+
+    private Vector3 _normalAttackOffset = Vector3.forward;
     private Vector3 _awakenAttackOffset;
 
     private bool _isEffectOn = false;
@@ -47,7 +45,7 @@ public class PlayerNormalChargeAttackState : PlayerChargeState
     public override void Exit()
     {
         base.Exit();
-
+        _player.enemyNormalHitDuplicateChecker.Clear();
     }
 
     public override void UpdateState()
@@ -55,43 +53,43 @@ public class PlayerNormalChargeAttackState : PlayerChargeState
         base.UpdateState();
         
         // 가설. 여기서 이게 계속 돎 그렇기에 때리는 함수에 값을 넘기고 
-        ChargeAttack();
+        if (_isHitAbleTriggerCalled)
+            ChargeAttack();
 
-        ChargeAttackEffect();
-
-
-        if (_endTriggerCalled)                                                                                
-        {
-            _stateMachine.ChangeState(PlayerStateEnum.NormalChargeStabAttack);
-        }
-    }
-
-    private void ChargeAttackEffect()
-    {
         if (_effectTriggerCalled)
         {
             if (!_isEffectOn)
             {
                 _isEffectOn = true;
-
-                Vector3 pos = _player._weapon.transform.position;
-                EffectManager.Instance.PlayEffect(PoolingType.Effect_PlayerAttack_Charged_Normal, pos);
+                ChargeAttackEffect();
             }
         }
+
+        if (_endTriggerCalled)                                                                                
+        {
+            _stateMachine.ChangeState(PlayerStateEnum.Idle);
+        }
+    }
+
+    private void ChargeAttackEffect()
+    {
+        Vector3 pos = _player._weapon.transform.position;
+        EffectManager.Instance.PlayEffect(PoolingType.Effect_PlayerAttack_Charged_Normal, pos);
+
     }
 
     private void ChargeAttack()
     {
-        Vector3 offset = new Vector3(_player.transform.forward.x, _player.transform.forward.y, _player.transform.forward.z);
-        offset.x *= _attackOffset.x;
-        offset.y *= _attackOffset.y;
-        offset.z *= _attackOffset.z;
+        Vector3 offset = _player.transform.forward;
+        offset.x += _attackOffset.x;
+        offset.y += _attackOffset.y;
+        offset.z += _attackOffset.z;
 
-        Vector3 pos = _player._weapon.transform.position + offset;
+        Vector3 pos = _weaponRT.position + offset;
 
         Vector3 halfSize = _attackSize * 0.5f;
 
-        Quaternion rot = Quaternion.Euler((_player.transform.forward * _hitDist) - _player.transform.position);
+        Quaternion rot = Quaternion.Euler((_player.transform.forward * _hitDist) - _player.transform.position).normalized;
         
         Collider[] enemies = Physics.OverlapBox(pos, halfSize, rot, _player._enemyLayer);
 
