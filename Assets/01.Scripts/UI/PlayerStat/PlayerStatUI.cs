9eff9f233ce1b4ecf9b946a273018c711648a946
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-06-02 14:58:52 +0900

    Fix SkillUI

--- Assets/01.Scripts/UI/PlayerStat/PlayerStatUI.cs
+++ Assets/01.Scripts/UI/PlayerStat/PlayerStatUI.cs
@@ -10,11 +10,52 @@ public class PlayerStatUI : MonoBehaviour
     public Slider playerAwakenGage;
     public Slider playerChargeGage;
 
+    [Header("PlayerSkill")]
+    public Image QSkill;
+    public Image MouseLeftSkill;
+    public Image DefenceSkill;
+
     private void Update()
     {
         UpdateHp();
         UpdateAwakenGage();
         UpdateChargeGage();
+        
+        if(!GameManager.Instance.PlayerInstance.IsAwakening)
+        {
+            if (GameManager.Instance.PlayerInstance.StateMachine.CompareState(PlayerStateEnum.NormalChargeAttack))
+            {
+                StartCoroutine(CoolTime(MouseLeftSkill, GameManager.Instance.PlayerInstance.PlayerStatData.chargingAttackCooldown.GetValue()));
+            }
+
+            if (GameManager.Instance.PlayerInstance.StateMachine.CompareState(PlayerStateEnum.NormalSlam))
+            {
+                StartCoroutine(CoolTime(QSkill, GameManager.Instance.PlayerInstance.PlayerStatData.slamCooldown.GetValue()));
+            }
+
+            if (GameManager.Instance.PlayerInstance.StateMachine.CompareState(PlayerStateEnum.Defense))
+            {
+                StartCoroutine(CoolTime(DefenceSkill, GameManager.Instance.PlayerInstance.PlayerStatData.defenseCooldown.GetValue()));
+            }
+        }
+        else
+        {
+            if (GameManager.Instance.PlayerInstance.StateMachine.CompareState(PlayerStateEnum.AwakenChargeAttack))
+            {
+                StartCoroutine(CoolTime(MouseLeftSkill, GameManager.Instance.PlayerInstance.PlayerStatData.chargingAttackCooldown.GetValue()));
+            }
+
+            if (GameManager.Instance.PlayerInstance.StateMachine.CompareState(PlayerStateEnum.AwakenSlam))
+            {
+                StartCoroutine(CoolTime(QSkill, GameManager.Instance.PlayerInstance.PlayerStatData.slamCooldown.GetValue()));
+            }
+
+            if (GameManager.Instance.PlayerInstance.StateMachine.CompareState(PlayerStateEnum.Defense))
+            {
+                StartCoroutine(CoolTime(DefenceSkill, GameManager.Instance.PlayerInstance.PlayerStatData.defenseCooldown.GetValue()));
+            }
+        }
+
     }
 
     public void UpdateHp()
@@ -30,5 +71,20 @@ public class PlayerStatUI : MonoBehaviour
     public void UpdateChargeGage()
     {
         playerChargeGage.value = GameManager.Instance.PlayerInstance.ChargingGauge;
+    }
+
+    IEnumerator CoolTime(Image skillImage, float cooldown)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < cooldown)
+        {
+            elapsed += Time.deltaTime;
+            skillImage.fillAmount = Mathf.Clamp01(1 - (elapsed / cooldown));
+
+            yield return null;
+        }
+
+        skillImage.fillAmount = 0f;
     }
 }
