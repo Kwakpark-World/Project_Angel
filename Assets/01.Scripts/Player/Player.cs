39b55b204da1dd5a7485fc6cda9b83268f4f655a
김준호 <kjhlove0119@naver.com>
2024-06-17 16:18:34 +0900

    REFECTOR caching

--- Assets/01.Scripts/Player/Player.cs
+++ Assets/01.Scripts/Player/Player.cs
@@ -65,7 +65,6 @@ public class Player : PlayerController
     {
         base.Awake();
 
-        //MaterialManager.Instance._cachingAction += MaterialCaching;
         MaterialCaching();
 
         BuffCompo.SetOwner(this);
