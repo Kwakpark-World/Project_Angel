a738edf9a189cf74f27dbb737f7b7811eb96eff4
Taegon Lee <ltg9amer@naver.com>
2024-06-25 10:37:51 +0900

    FIX_RuneList

--- Assets/01.Scripts/UI/Popup/CreditPopupUI.cs
+++ Assets/01.Scripts/UI/Popup/CreditPopupUI.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlSettingPopupUI : PopupUI
+public class CreditPopupUI : PopupUI
 {
-    // Create key mapping feature in here.
-
     public override void InitializePopup()
     {
-        // Get saved key mapping data.
+        // Do tween if necessary.
     }
 }
