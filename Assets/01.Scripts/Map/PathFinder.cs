ce9e970d50804cd77556e75a574037149fe043c8
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-06-26 18:51:29 +0900

    Fix_PathFinder

--- Assets/01.Scripts/Map/PathFinder.cs
+++ Assets/01.Scripts/Map/PathFinder.cs
@@ -33,14 +33,12 @@ public class PathFinder : MonoBehaviour
         {
             if (_targetIndex >= _targetList.Count)
             {
-                Debug.Log("No more targets.");
                 _lineRenderer.enabled = false;
                 break;
             }
 
-            if (Vector3.Distance(transform.position, _targetList[_targetIndex].position) <= 2f)
+            if (Vector3.Distance(transform.position, _targetList[_targetIndex].position) <= 5f)
             {
-                Debug.Log("Reached target " + _targetIndex);
                 PassToNextDestination();
             }
 
@@ -49,12 +47,10 @@ public class PathFinder : MonoBehaviour
                 Debug.Log(UIManager.Instance._mode);
                 if (UIManager.Instance._mode == BGMMode.Combat)
                 {
-                    Debug.Log("Combat mode: Clearing path.");
                     _lineRenderer.positionCount = 0;
                 }
                 else if (UIManager.Instance._mode == BGMMode.NonCombat)
                 {
-                    Debug.Log("NonCombat mode: Drawing path.");
                     DrawPathToCurrentTarget();
                 }
             }
@@ -66,32 +62,23 @@ public class PathFinder : MonoBehaviour
     public void PassToNextDestination()
     {
         _targetIndex++;
-        Debug.Log("Passed to next target: " + _targetIndex);
     }
 
     private void DrawPathToCurrentTarget()
     {
-        Debug.Log("Inside DrawPathToCurrentTarget");
         if (_targetIndex < _targetList.Count)
         {
-            Debug.Log("Drawing path to target " + _targetIndex);
             Vector3[] pathCorners = new Vector3[2];
             pathCorners[0] = transform.position;
             pathCorners[1] = _targetList[_targetIndex].position;
             _lineRenderer.positionCount = pathCorners.Length;
             _lineRenderer.SetPositions(pathCorners);
-            _lineRenderer.enabled = true;  // Ensure the LineRenderer is enabled
-            Debug.Log("Path drawn to target " + _targetIndex);
-        }
-        else
-        {
-            Debug.Log("Target index out of bounds: " + _targetIndex);
+            _lineRenderer.enabled = true; 
         }
     }
 
     public void SetMode(BGMMode newMode)
     {
         _mode = newMode;
-        Debug.Log("Mode set to " + _mode);
     }
 }
