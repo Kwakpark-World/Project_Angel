3ee1caf3912fa47498cb0f121028f41b4e0a4b3c
rhkrhyunmin <rhkrhyunmin@gmail.com>
2024-06-26 18:16:06 +0900

    Fix_PathFinder

--- Assets/01.Scripts/Map/PathFinder.cs
+++ Assets/01.Scripts/Map/PathFinder.cs
@@ -7,7 +7,7 @@ public class PathFinder : MonoBehaviour
 {
     private LineRenderer _lineRenderer;
 
-    public List<Transform> _targetList; // 시작을 0
+    public List<Transform> _targetList;
     private int _targetIndex;
 
     private BGMMode _mode;
@@ -33,18 +33,30 @@ public class PathFinder : MonoBehaviour
         {
             if (_targetIndex >= _targetList.Count)
             {
+                Debug.Log("No more targets.");
                 _lineRenderer.enabled = false;
                 break;
             }
 
             if (Vector3.Distance(transform.position, _targetList[_targetIndex].position) <= 2f)
             {
+                Debug.Log("Reached target " + _targetIndex);
                 PassToNextDestination();
             }
 
             if (_targetIndex < _targetList.Count)
             {
-                DrawPathToCurrentTarget();
+                Debug.Log(UIManager.Instance._mode);
+                if (UIManager.Instance._mode == BGMMode.Combat)
+                {
+                    Debug.Log("Combat mode: Clearing path.");
+                    _lineRenderer.positionCount = 0;
+                }
+                else if (UIManager.Instance._mode == BGMMode.NonCombat)
+                {
+                    Debug.Log("NonCombat mode: Drawing path.");
+                    DrawPathToCurrentTarget();
+                }
             }
 
             yield return delayTime;
@@ -54,22 +66,32 @@ public class PathFinder : MonoBehaviour
     public void PassToNextDestination()
     {
         _targetIndex++;
+        Debug.Log("Passed to next target: " + _targetIndex);
     }
 
     private void DrawPathToCurrentTarget()
     {
-        if(_mode == BGMMode.NonCombat)
+        Debug.Log("Inside DrawPathToCurrentTarget");
+        if (_targetIndex < _targetList.Count)
         {
-            if (_targetIndex < _targetList.Count)
-            {
-                Vector3[] pathCorners = new Vector3[2];
-                pathCorners[0] = transform.position;
-                pathCorners[1] = _targetList[_targetIndex].position;
-
-                _lineRenderer.positionCount = pathCorners.Length;
-                _lineRenderer.SetPositions(pathCorners);
-            }
+            Debug.Log("Drawing path to target " + _targetIndex);
+            Vector3[] pathCorners = new Vector3[2];
+            pathCorners[0] = transform.position;
+            pathCorners[1] = _targetList[_targetIndex].position;
+            _lineRenderer.positionCount = pathCorners.Length;
+            _lineRenderer.SetPositions(pathCorners);
+            _lineRenderer.enabled = true;  // Ensure the LineRenderer is enabled
+            Debug.Log("Path drawn to target " + _targetIndex);
         }
- 
+        else
+        {
+            Debug.Log("Target index out of bounds: " + _targetIndex);
+        }
+    }
+
+    public void SetMode(BGMMode newMode)
+    {
+        _mode = newMode;
+        Debug.Log("Mode set to " + _mode);
     }
 }
